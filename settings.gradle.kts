f118bd192cbe9a012430a0f625cc41178ceac132
ThiagoROX <thiagokenis@live.com>
2025-07-18 22:19:08 -0300

    Update dependencies and project versions for 1.21.7
    Bump project version to 1.28 and update apiVersion and mcVersion to 1.21 and 1.21.7, respectively. Upgrade several dependencies in libs.versions.toml and settings.gradle.kts, including kyori, runpaper, decentholograms, triumphgui, nbtapi, and oraxen. Reorder and clean up repository declarations in settings.gradle.kts.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,25 +1,23 @@
 pluginManagement {
     repositories {
-        maven("https://repo.papermc.io/repository/maven-public/")
-
         gradlePluginPortal()
         mavenCentral()
+        maven("https://repo.papermc.io/repository/maven-public/")
     }
 }
 
 dependencyResolutionManagement {
     repositories {
-        maven("https://repo.papermc.io/repository/maven-public/")
-
         gradlePluginPortal()
         mavenCentral()
+        maven("https://repo.papermc.io/repository/maven-public/")
     }
 
     versionCatalogs {
         create("libs") {
             version("cluster", "6.4")
 
-            library("decentholograms", "com.github.decentsoftware-eu", "decentholograms").version("2.8.11")
+            library("decentholograms", "com.github.decentsoftware-eu", "decentholograms").version("2.9.6")
 
             library("triumphcmds", "dev.triumphteam", "triumph-cmd-bukkit").version("2.0.0-SNAPSHOT")
 
@@ -28,11 +26,11 @@ dependencyResolutionManagement {
 
             library("placeholderapi", "me.clip", "placeholderapi").version("2.11.6")
 
-            library("triumphgui", "dev.triumphteam", "triumph-gui").version("3.1.10")
+            library("triumphgui", "dev.triumphteam", "triumph-gui").version("3.1.11")
 
-            library("nbtapi", "de.tr7zw", "item-nbt-api").version("2.13.2")
+            library("nbtapi", "de.tr7zw", "item-nbt-api").version("2.15.1")
 
-            library("oraxen", "io.th0rgal", "oraxen").version("1.181.0")
+            library("oraxen", "io.th0rgal", "oraxen").version("1.191.0")
 
             library("configme", "ch.jalu", "configme").version("1.4.1")
         }
